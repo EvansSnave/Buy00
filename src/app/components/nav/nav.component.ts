347261fc3467c1ada33b0baeca358fffca56c064
EvansSnave <127534196+EvansSnave@users.noreply.github.com>
2024-07-02 18:32:57 -0500

    Add mixins

---
+++ src/app/components/nav/nav.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'navApp',
+  standalone: true,
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.scss']
+})
+export class NavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
