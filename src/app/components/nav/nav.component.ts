cbc284a49a3c66afb859ea1004a99e213cfb6e96
EvansSnave <127534196+EvansSnave@users.noreply.github.com>
2024-08-02 13:35:05 -0500

    Add main padding

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,10 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { MainButtonComponent } from '../main-button/main-button.component';
 
 @Component({
   selector: 'nav-app',
   standalone: true,
-  imports: [MainButtonComponent],
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss']
 })
